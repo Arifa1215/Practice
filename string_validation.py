9e7f2c1c6639a9af5c2562d1129d340deb9a683c
Arifa <arifaazar786@gmail.com>
2025-01-06 06:20:57 +0530

    Add files via upload

---
+++ string_validation.py
@@ -0,0 +1,20 @@
+UPPERCASE_LETTER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def is_valid_string(s):
+    for char in s:
+        result = (char in UPPERCASE_LETTER) or (char == "_")
+        if result is False:
+            return False
+    return True
+
+def main():
+    input_str = input()
+    is_valid = is_valid_string(input_str)
+
+    if is_valid:
+        result = "True " + str(len(input_str.split("_")))
+    else:
+        result = "False"
+    print(result)
+
+main()
